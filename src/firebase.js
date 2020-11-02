88d3de826bca6cc73619f3fefc093c9e951af6b2
Cengizhan KOSE <kose651@gmail.com>
2020-11-03 01:37:38 +0300

    firebase config add

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAR6QSlY9feFSJuMnLaMVRbowfnkKgBhLE",
+  authDomain: "whatsapp-mern-clo.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-clo.firebaseio.com",
+  projectId: "whatsapp-mern-clo",
+  storageBucket: "whatsapp-mern-clo.appspot.com",
+  messagingSenderId: "463033998935",
+  appId: "1:463033998935:web:02bce2383c4a547e934d12",
+};
