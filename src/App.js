9f8a007f4b263f40a04f5e04974869d3c373b81b
Cengizhan KOSE <kose651@gmail.com>
2020-11-04 12:09:31 +0300

    api connections update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Chat } from "./Chat";
 import { Sidebar } from "./Sidebar";
+import Pusher from "pusher-js";
+import axios from "./axios";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get("/messages/sync").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    const pusher = new Pusher("70949a70c894df6fa302", {
+      cluster: "eu",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (newMessage) => {
+      setMessages([...messages, newMessage]);
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+
+  console.log(messages);
+
   return (
     <div className="app">
       <div className="app_body">
         <Sidebar />
-        <Chat />
+        <Chat messages={messages} />
       </div>
     </div>
   );
